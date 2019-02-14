606608beab85096c394ec697576f6dbb95aeb8dd
Jim Wang <yutsung.jim.wang@intel.com>
2019-02-14 16:22:52 -0700

    Add cleanup before seal to avoid the issue of mismatch parent handle if vault is reboot for some reason
    Signed-off-by: Jim Wang <yutsung.jim.wang@intel.com>

--- core/tpmSecretHandlerImpl.go
+++ core/tpmSecretHandlerImpl.go
@@ -22,6 +22,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/hashicorp/vault/api"
@@ -40,6 +43,10 @@ const (
 
 // SealVaultSecrets seals the secret data with TPM device
 func SealVaultSecrets(secretDataBytes []byte, encryptedOutputFile string) error {
+	// cleanup the left-over sealed file under the same directory of encryptedOutputFile
+	// before we do a new seal
+	cleanup(encryptedOutputFile)
+
 	// use ECC as public key template type for its much better performance on creating primary keys
 	templateType := "ecc"
 	//secretData := string(secretDataBytes)
@@ -169,4 +176,41 @@ func getTPMDevice() *util.TPMDevice {
 		tpmDevice = util.NewTPMDevice(&tpmPath)
 	}
 	return tpmDevice
+}
+
+func cleanup(file string) {
+	parentHandleFileName := util.GetTPMParentHandleFileName(file)
+	if _, err := os.Stat(parentHandleFileName); err == nil {
+		// parent handle exists, cleans it up
+		parentHandle := uint32(util.RetrieveParentHandle(parentHandleFileName))
+		// flush the TPM parentHandle context if any
+		if flushErr := util.FlushContext(getTPMDevice(), &parentHandle); flushErr != nil {
+			log.Printf("cannot flush TPM context for parentHandle: 0x%x", parentHandle)
+		}
+		// clean up the parentHandle file:
+		os.Remove(parentHandleFileName)
+	}
+
+	// clean up the secret blob files:
+	dir, _ := os.Getwd()
+	cleanupSealedFiles(dir)
+}
+
+func cleanupSealedFiles(pathToCleanup string) {
+	filepath.Walk(pathToCleanup, deletePrvFiles)
+	filepath.Walk(pathToCleanup, deletePubFiles)
+}
+
+func deletePrvFiles(path string, fileInfo os.FileInfo, err error) (e error) {
+	if strings.HasSuffix(fileInfo.Name(), ".prv") {
+		os.Remove(path)
+	}
+	return
+}
+
+func deletePubFiles(path string, fileInfo os.FileInfo, err error) (e error) {
+	if strings.HasSuffix(fileInfo.Name(), ".pub") {
+		os.Remove(path)
+	}
+	return
 }
